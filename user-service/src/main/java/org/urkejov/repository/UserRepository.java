2b586c5e9bdc2c75a71f6839962a4c25555e94ee
urkejov1996 <urkejov1996@gmail.com>
2024-08-01 07:58:29 +0200

    added user repository, fixed pom parent bug

--- user-service/src/main/java/org/urkejov/repository/UserRepository.java
+++ user-service/src/main/java/org/urkejov/repository/UserRepository.java
@@ -1,7 +1,18 @@
 package org.urkejov.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 import org.urkejov.entity.User;
 
+import java.util.Optional;
+
 @Repository
-public interface UserRepository extends JpaRepository<User, String>{
+public interface UserRepository extends JpaRepository<User, String> {
+
+    @Query("select u from User u where u.email = ?1")
+    Optional<User> findUserByEmail(String email);
+
+    @Query("select (count(u) > 0) from User u where u.email = ?1")
+    Boolean existsByEmail(String email);
 }
