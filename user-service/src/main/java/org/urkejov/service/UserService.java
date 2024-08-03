b0070dd98154c91fdfc7df4fdd4348c4aba36b58
urkejov1996 <urkejov1996@gmail.com>
2024-08-03 16:30:18 +0200

    added user controller, role tools and jwt tools

--- user-service/src/main/java/org/urkejov/service/UserService.java
+++ user-service/src/main/java/org/urkejov/service/UserService.java
@@ -1,14 +1,22 @@
 package org.urkejov.service;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.urkejov.repository.UserRepository;
 
 @Service
+@RequiredArgsConstructor
 public class UserService {
 
+    private final UserRepository userRepository;
 
     public ResponseEntity<?> getUser(String userId) {
+
+
+
+
         return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
     }
 }
