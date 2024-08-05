f7a03cb7c7c00e8491ee81a8013113a4269cf01a
urkejov1996 <urkejov1996@gmail.com>
2024-08-05 08:05:28 +0200

    added info message tools

--- user-service/src/main/java/org/urkejov/service/UserService.java
+++ user-service/src/main/java/org/urkejov/service/UserService.java
@@ -2,8 +2,6 @@ package org.urkejov.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
---
+++ user-service/src/main/java/org/urkejov/tools/InfoMessage.java
@@ -0,0 +1,6 @@
+package org.urkejov.tools;
+
+public class InfoMessage {
+    public static final String SUCCESS = "Action completed successfully.";
+    public static final String DELETED = "Resource deleted successfully.";
+}
